388149f93764c9e8728cc90e57dc80bc4ba4b538
Chad <chadn737@gmail.com>
2017-04-08 23:24:28 -0400

    Update scripts

--- scripts/cuffquant.sh
+++ scripts/cuffquant.sh
@@ -12,6 +12,7 @@ module load python/2.7.8
 module load boost/1.47.0/gcc447
 module load samtools/0.1.19
 module load cufflinks/2.2.1
+
 #Cuffquant
 echo "Running cuffquant"
-time cuffquant -p 10 -b ../../ref/bowtie2/Tguttata_v3.2.4.fa -M ../../ref/misc/Tguttata_v3.2.4_mask.gff -u --library-type fr-firststrand ../../ref/transcriptome/Tguttata_v3.2.4.gff tophat/accepted_hits.bam -o cuffquant
+time cuffquant -p 10 -b ../../ref/bowtie2/Tguttata_v3.2.4.fa -M ../../ref/misc/Tguttata_v3.2.4_mask.gff -u --library-type fr-firststrand ../../ref/transcriptome/Tguttata_v3.2.4.gff tophat/accepted_hits.bam -o cuffquant  
--- scripts/trim_align_rnaseq.sh
+++ scripts/trim_align_rnaseq.sh
@@ -14,31 +14,37 @@ module load fastqc/0.11.4
 mkdir raw_QC trimmed_QC
 
 #Unpack fastqs
+echo "Unpacking fastq files"
 cd ../fastq/rnaseq
-for i in *gz
-do
-gunzip "$i"
-done
-cat *.fastq > all.fastq
+if [ -s trimmed.fastq.gz ]
+then
+  gunzip trimmed.fastq.gz
+  cd ../../
+else
+  for i in *gz
+  do
+    gunzip "$i"
+  done
+  cat *.fastq > all.fastq
+  #QC analysis raw data
+  echo "First QC analysis"
+  time fastqc --noextract -t 10 -a ../../../../misc/TruSeq.tsv -o ../../rnaseq/raw_QC all.fastq
 
-#QC analysis raw data
-echo "First QC analysis"
-time fastqc --noextract -t 10 -a ../../../../misc/TruSeq.tsv -o ../../rnaseq/raw_QC all.fastq
+  #Trim data
+  echo "Trimming data"
+  python /usr/local/apps/cutadapt/1.9.dev1/bin/cutadapt --trim-n --max-n 0.1 -m 30 -q 20,20 -e 0.1 -O 3 -a file:../../../../misc/TruSeq.fa -o trimmed.fastq all.fastq
+  rm all.fastq
 
-#Trim data
-echo "Trimming data"
-python /usr/local/apps/cutadapt/1.9.dev1/bin/cutadapt --trim-n --max-n 0.1 -m 30 -q 20,20 -e 0.1 -O 3 -a file:../../../../misc/TruSeq.fa -o trimmed.fastq all.fastq
-rm all.fastq
-
-#QC analysis trimmed data
-echo "Second QC analysis"
-time fastqc --noextract -t 10 -a ../../../../misc/TruSeq.tsv -o ../../rnaseq/trimmed_QC trimmed.fastq
-cd ../../
+  #QC analysis trimmed data
+  echo "Second QC analysis"
+  time fastqc --noextract -t 10 -a ../../../../misc/TruSeq.tsv -o ../../rnaseq/trimmed_QC trimmed.fastq
+  cd ../../
+fi
 
 #Align data
-echo "running tophat"
+echo "Running tophat"
 cd rnaseq
-time tophat -F 0.1 --transcriptome-index ../../ref/transcriptome/Tguttata_v3.2.4 --no-coverage-search --b2-sensitive --no-mixed --read-realign-edit-dist 0 -i 70 -M -I 500000 -p 10 --library-type fr-firststrand -o tophat ../../ref/bowtie2/Tguttata_v3.2.4 ../fastq/rnaseq/trimmed.fastq
+time tophat -F 0.1 --transcriptome-index ../../ref/transcriptome/Tguttata_v3.2.4 --no-coverage-search --b2-very-sensitive --no-mixed --read-realign-edit-dist 0 -i 70 -M -I 500000 -p 10 --library-type fr-firststrand -o tophat ../../ref/bowtie2/Tguttata_v3.2.4 ../fastq/rnaseq/trimmed.fastq
 cd ../
 
 #Repackage everything
@@ -46,7 +52,8 @@ echo "Wrapping up"
 cd fastq/rnaseq
 for i in *fastq
 do
-gzip "$i"
+  gzip "$i"
 done
+cd ../../
 
 echo "Done"

3dbab2e334f209a342692eb9fb04b442d3dfce08
chadn737 <chadn737@gmail.com>
2017-05-29 01:33:17 -0400

    trying to merge

--- scripts/salmon.sh
+++ scripts/salmon.sh
@@ -5,11 +5,17 @@
 #PBS -l walltime=480:00:00
 #PBS -l mem=10gb
 
-echo "Unpacking fastq files"
+cd $PBS_O_WORKDIR
+
 cd ../fastq/rnaseq
 if [ -s trimmed2.fastq.gz ]
 then
+  echo "Unpacking fastq"
   gunzip trimmed2.fastq.gz
+  cd ../../rnaseq
+elif [ -s trimmed2.fastq ]
+then
+  echo "trimmed2.fastq exists"
   cd ../../rnaseq
 else
   echo "trimmed2.fastq not found"
@@ -17,13 +23,17 @@ else
 fi 
 
 module load salmon/0.8.2
-salmon quant -p 10 --posBias --seqBias --gcBias --fldMean 200 --fieldSD 20 \
+salmon quant -p 10 --posBias --seqBias --gcBias --fldMean 200 --fldSD 20 \
 --numBiasSamples 10000000 --numGibbsSamples 10 \
 -i ../../ref/transcriptome/Tguttata_index -l SR \
 -r ../fastq/rnaseq/trimmed2.fastq -o salmon_gibbs
 
-salmon quant -p 10 --posBias --seqBias --gcBias --fldMean 200 --fieldSD 20 \
+salmon quant -p 10 --posBias --seqBias --gcBias --fldMean 200 --fldSD 20 \
 --numBiasSamples 10000000 --numBootstraps 10 \
 -i ../../ref/transcriptome/Tguttata_index -l SR \
 -r ../fastq/rnaseq/trimmed2.fastq -o salmon_boots 
+
+cd ../fastq/rnaseq
+gzip trimmed2.fastq
+cd ../../rnaseq
 

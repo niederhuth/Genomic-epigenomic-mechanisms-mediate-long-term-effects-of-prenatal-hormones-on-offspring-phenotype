513189db7848a7291a276a7b1427757ff1b266ec
Chad Niederhuth <niederhu@msu.edu>
2018-11-30 21:02:33 -0500

    stuff

--- scripts/DMR.sh
+++ scripts/DMR.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=10
 #SBATCH --mem=100GB
 #SBATCH --job-name DMRfind
 #SBATCH --output=job_reports/%x-%j.SLURMout
@@ -16,8 +16,7 @@ SAMPLES='MCH1 MCH2 MCH3 MTH1 MTH2 MTH3'
 
 SAMPLE_CATEGORY=$(echo $(for i in $(seq 1 $(expr $(echo $SAMPLES | wc -w) / 3)); do printf "%0.s$i, " {1..3}; done) | sed s/,$//)
 
-#CHROMS='1 10 11 12 13 14 15 17 18 19 1A 1B 2 20 21 22 23 24 25 26 27 28 3 4 4A 5 6 7 8 9 LG2 LG5 LGE22 Z Un 4_random 8_random Z_random 13_random 5_random 6_random 21_random 2_random 26_random 3_random 1_random 22_random 1A_random 7_random 10_random 23_random 18_random 25_random LGE22_random 9_random 15_random 2_random 20_random 11_random 4A_random 14_random 17_random 27_random 19_random 28_random 16_random 24_random 1B_random'
-CHROMS='1'
+CHROMS='1 10 11 12 13 14 15 17 18 19 1A 1B 2 20 21 22 23 24 25 26 27 28 3 4 4A 5 6 7 8 9 LG2 LG5 LGE22 Z Un 4_random 8_random Z_random 13_random 5_random 6_random 21_random 2_random 26_random 3_random 1_random 22_random 1A_random 7_random 10_random 23_random 18_random 25_random LGE22_random 9_random 15_random 2_random 20_random 11_random 4A_random 14_random 17_random 27_random 19_random 28_random 16_random 24_random 1B_random'
 
 ALLC_FILES=$(for i in $SAMPLES; do awk -v a=$i 'BEGIN {print "../"a"/methylCseq/allc_"a".tsv.gz"}'; done | tr '\n' ' ')
 
@@ -32,14 +31,14 @@ do
 	--output-prefix $i \
 	--chroms $CHROMS \
 	--mc-type $i \
-	--num-procs 6 \
+	--num-procs 10 \
 	--min-cov 0 \
-	--dmr-max-dist 250 \
+	--dmr-max-dist 500 \
 	--sig-cutoff 0.01 \
 	--num-sims 3000 \
 	--min-tests 100 \
 	--min-num-dms 0 \
-	--mc-max-dist 200 \
+	--mc-max-dist 500 \
 	--keep-temp-files False \
 	--min-cluster 2 \
 	--seed -1 \
